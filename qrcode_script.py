7fca6e1740e2719498f8157c53ed6518c51c90f2
turbo976-sys <abakshay050@gmail.com>
2025-09-08 17:35:31 +0530

    Create qrcode_script.py
    beginner level project

---
+++ qrcode_script.py
@@ -0,0 +1,7 @@
+data = input("Enter a text url: ").strip()
+filename = input("enter the a filename :").strip()
+qr = qrcode.QRCode(box_size = 10, border = 4)
+qr.add_data(data)
+image = qr.make_image(fill_color= "black",back_color="white")
+image.save(filename)
+print(f"QR code saved as {filename}")
